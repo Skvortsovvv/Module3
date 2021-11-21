9894bfc1f823cdae7a7473aa02cf02962accea12
Vladimir <vladimirs001@yandex.ru>
2021-11-21 16:33:37 +0300

    fix1

--- A.py
+++ A.py
@@ -17,22 +17,25 @@ class Bag:
 
     def calculate(self):
         previous = [[0, []] for _ in range(0, self.capacity+1)]
-        current = previous
+        current = [[0, []] for _ in range(0, self.capacity+1)]
         # массив вида current[i] = [value, [indexes]], где value - ценность всего рюкзака
         # [indexes] - индексы предметов, находящихся в рюкзаке при данной ценности
         # i - вес рюкзака
         for i in range(1, len(self.items)):  # индексы предметов
             for j in range(1, self.capacity+1):  # индексы весов рюкзака
-                if self.items[i][0] > self.capacity:
+                if self.items[i][0] > j:
                     current[j] = previous[j]
                 else:
-                    value = max(previous[j][0], self.items[i][1] + previous[j-1][0])
-                    if previous[j] == value:
-                        current[i] = previous[j]
+                    value = max(previous[j][0], self.items[i][1] + previous[j-self.items[i][0]][0])
+                    if previous[j][0] == value:
+                        current[j] = previous[j]
                     else:
-                        current[j][0] = self.items[i][1] + previous[j-1][0]
+                        current[j][0] = value
+                        current[j][1] = [e for e in previous[j-self.items[i][0]][1]]
                         current[j][1].append(i)
-            current, previous = previous, current
+            previous = current
+            if i != len(self.items)-1:
+                current = [[0, []] for _ in range(0, self.capacity+1)]
 
         result_weight = 0
         for index in current[self.capacity][1]:
@@ -41,17 +44,24 @@ class Bag:
 
 
 if __name__ == "__main__":
-    bag = Bag(165)
-    bag.add_item(23, 92)
-    bag.add_item(31, 92)
-    bag.add_item(29, 49)
-    bag.add_item(44, 68)
-    bag.add_item(53, 60)
-    bag.add_item(38, 43)
-    bag.add_item(63, 67)
-    bag.add_item(85, 84)
-    bag.add_item(89, 87)
-    bag.add_item(82, 72)
+    bag = Bag(3)
+    bag.add_item(3, 55)
+    bag.add_item(2, 80)
+    bag.add_item(4, 60)
+    bag.add_item(1, 45)
+
+
+    # bag = Bag(165)
+    # bag.add_item(23, 92)
+    # bag.add_item(31, 92)
+    # bag.add_item(29, 49)
+    # bag.add_item(44, 68)
+    # bag.add_item(53, 60)
+    # bag.add_item(38, 43)
+    # bag.add_item(63, 67)
+    # bag.add_item(85, 84)
+    # bag.add_item(89, 87)
+    # bag.add_item(82, 72)
     result = bag.calculate()
     print(result)
 
