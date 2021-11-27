0fbe7efe38004c0a54c29fcb1fc3576bee3035c6
Vladimir <vladimirs001@yandex.ru>
2021-11-27 14:20:43 +0300

    mee

--- A_new2.py
+++ A_new2.py
@@ -14,18 +14,6 @@ class Bag:
     def add_item(self, weight, price):
         self.items.append([[weight, price]])
 
-    # def make_combines(self, row, weights):
-    #     for i in range(2, len(self.items)+1):
-    #         comb = combinations(weights, i)
-    #         for c in comb:
-    #             comb_weight = 0
-    #             for ci in c:
-    #                 comb_weight += ci
-    #             if comb_weight <= self.capacity:
-    #                 if comb_weight not in row:
-    #                     row[comb_weight] = [0, []]
-    #     return row
-
     def make_weights(self):
         weights = {}
         for i in range(0, len(self.items)):
@@ -46,30 +34,52 @@ class Bag:
 
     def recursive(self, bag_weight, item_weight, index, weights, limit=None):
         if bag_weight-item_weight not in weights.keys():
-            if bag_weight - item_weight >= self.items[0][0][0]:
+            if bag_weight not in weights.keys():
+                weights[bag_weight] = {}
+                weights[bag_weight][index] = [0, []]
+            if item_weight == 0:
+                weights[bag_weight][index] = [0, []]
+            elif bag_weight - item_weight >= self.items[1][0][0]:
                 self.recursive(bag_weight-item_weight, item_weight, index, weights)
+                self.recursive(bag_weight, item_weight, index, weights)
+            elif bag_weight == 0:
+                weights[bag_weight][index] = [0, []]
             else:
-                weights[bag_weight][index] = weights[bag_weight][index-1]
+                _prev_v = weights[bag_weight][index-1][0]
+                _sum = self.items[index][0][1]
+                if _sum > _prev_v:
+                    weights[bag_weight][index] = [_sum, []]
+                    # weights[bag_weight][index][1].append(self.items[index][1][0])
+                    weights[bag_weight][index][1].append(index)
+                else:
+                    weights[bag_weight][index] = weights[bag_weight][index-1]
+
             if limit != bag_weight:
                 self.recursive(bag_weight, item_weight, index, weights)
         else:
             if bag_weight not in weights.keys():
                 weights[bag_weight] = {}
+                weights[bag_weight][index] = [0, []]
             if index == 1:
-                weights[bag_weight][index] = [self.items[index][0][1], [self.items[index][1][0]]]
+                # weights[bag_weight][index] = [self.items[index][0][1], [self.items[index][1][0]]]
+                weights[bag_weight][index] = [self.items[index][0][1], [index]]
             elif index == 0:
                 weights[bag_weight][index] = [0, []]
             else:
                 sum = self.items[index][0][1] + weights[bag_weight-item_weight][index][0]
+                if index-1 not in weights[bag_weight]:
+                    self.recursive(bag_weight, self.items[index-1][0][0], index-1, weights)
                 prev_v = weights[bag_weight][index-1][0]
                 if sum > prev_v:
                     weights[bag_weight][index] = [sum, []]
                     weights[bag_weight][index][1] = \
                         [i for i in weights[bag_weight-item_weight][index][1]]
-                    if self.items[index][1][0] not in weights[bag_weight][index][1]:
-                        weights[bag_weight][index][1].append(self.items[index][1][0])
+                    # if self.items[index][1][0] not in weights[bag_weight][index][1]:
+                    if index not in weights[bag_weight][index][1]:
+                        # weights[bag_weight][index][1].append(self.items[index][1][0])
+                        weights[bag_weight][index][1].append(index)
                 else:
-                    weights[bag_weight][index] = weights[bag_weight][self.items[index-1][0][0]]
+                    weights[bag_weight][index] = weights[bag_weight][index-1]
         return
 
     def calculate(self):
@@ -88,81 +98,24 @@ class Bag:
             for bag_weight in weights.keys():
                 self.recursive(bag_weight, item_weight, i, weights, bag_weight)
 
+        print()
+
         for i in range(0, len(self.items)):
             item_weight = self.items[i][0][0]
             self.recursive(self.capacity, item_weight, i, weights, self.capacity)
 
-        result = weights[self.capacity]
+        result_weight = 0
+        result_value = weights[self.capacity][len(self.items)-1][0]
+        indexes = []
 
-        return result
+        for i in weights[self.capacity][len(self.items)-1][1]:
+            result_weight += self.items[i][0][0]
+            indexes.append(self.items[i][1][0])
+        indexes.sort()
 
-        # if self.capacity not in previous:
-        #     previous[self.capacity] = [0, []]
-        #     current[self.capacity] = [0, []]
-        #
-        # for i in range(1, len(self.items)):
-        #     for key in current.keys():
-        #         if self.items[i][0] > key:
-        #             current[key] = previous[key]
-        #         else:
-        #             v = self.items[i][1]
-        #
-        #             if key - self.items[i][0] in previous.keys():
-        #                 value = max(previous[key][0], v + previous[key - self.items[i][0]][0])
-        #
-        #                 if value == previous[key][0]:
-        #                     current[key] = previous[key]
-        #                 else:
-        #                     current[key][0] = value
-        #                     current[key][1] = [index for index in previous[key - self.items[i][0]][1]]
-        #                     current[key][1].append(i)
-        #             else:
-        #                 value = max(previous[key][0], v)
-        #
-        #                 if value == previous[key][0]:
-        #                     current[key] = previous[key]
-        #                 else:
-        #                     current[key][0] = value
-        #                     current[key][1].append(i)
-        #
-        #     previous = current
-        #     if i != len(self.items)-1:
-        #         current = {}
-        #         for k in previous.keys():
-        #             current[k] = [0, []]
+        return [result_weight, result_value, indexes]
 
-        # result_weight = 0
-        # for i in current[self.capacity][1]:
-        #     result_weight += self.items[i][0]
-        # return [result_weight, current[self.capacity][0], current[self.capacity][1]]
 
-        #
-        # previous = [[0, []] for _ in range(0, self.capacity+1)]
-        # current = [[0, []] for _ in range(0, self.capacity+1)]
-        # # массив вида current[i] = [value, [indexes]], где value - ценность всего рюкзака
-        # # [indexes] - индексы предметов, находящихся в рюкзаке при данной ценности
-        # # i - вес рюкзака
-        # for i in range(1, len(self.items)):  # индексы предметов
-        #     for j in range(1, self.capacity+1):  # индексы весов рюкзака
-        #         if self.items[i][0] > j:
-        #             current[j] = previous[j]
-        #         else:
-        #             value = max(previous[j][0], self.items[i][1] + previous[j-self.items[i][0]][0])
-        #             if previous[j][0] == value:
-        #                 current[j] = previous[j]
-        #             else:
-        #                 current[j][0] = value
-        #                 current[j][1] = [e for e in previous[j-self.items[i][0]][1]]
-        #                 current[j][1].append(i)
-        #     previous = current
-        #     if i != len(self.items)-1:
-        #         current = [[0, []] for _ in range(0, self.capacity+1)]
-        #
-        # result_weight = 0
-        # for index in current[self.capacity][1]:
-        #     result_weight += self.items[index][0]
-        # return [result_weight, current[self.capacity][0], current[self.capacity][1]]
-        #
 
 def process(b: Bag, item):
     if re.fullmatch(r'[\d+]+ [\d+]+', item):
@@ -177,7 +130,7 @@ def process(b: Bag, item):
 
 def main():
 
-    bag = Bag(5)
+    bag = Bag(8)
 
     bag.add_item(3, 55)
     bag.add_item(2, 80)
